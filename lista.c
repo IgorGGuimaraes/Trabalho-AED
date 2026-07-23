1ab1673cea4b1d49aeaa75ddeed53932bf818dd7
rhuannh <rhuanhenrique809@gmail.com>
2026-07-23 15:10:13 -0300

    Add functions to alter and remove genres

--- lista.c
+++ lista.c
@@ -3,6 +3,63 @@
 #include <string.h>
 #include "lista.h"
 
+//funçoes rhuan
+
+void alterarGenero(Genero *g, char nomeAtual[], char novoNome[]) {
+    Genero *atual = g;
+    while (atual != NULL) {
+        if (strcmp(atual->nome, nomeAtual) == 0) {
+            strcpy(atual->nome, novoNome);
+            printf("Genero alterado com sucesso!\n");
+            return;
+        }
+        atual = atual->prox;
+    }
+    printf("Genero nao encontrado.\n");
+}
+
+int removerGenero(Genero **g, char nome[]) {
+    Genero *atual = *g;
+    while (atual != NULL && strcmp(atual->nome, nome) != 0) {
+        atual = atual->prox;
+    }
+    if (atual == NULL) {
+        printf("Genero nao encontrado.\n");
+        return 0; }
+    if (atual->artistas != NULL) {
+        printf("Nao e possivel remover um genero que possui artistas cadastrados.\n");
+        return 0;
+    }
+    
+    //elemento unico da lista
+    if (atual->ant == NULL && atual->prox == NULL) {
+        *g = NULL;
+    }
+    
+    // removendo no inicio 
+    else if (atual->ant == NULL) {
+        *g = atual->prox;
+        atual->prox->ant = NULL;
+    }
+
+    // removendo no final
+    else if (atual->prox == NULL) {
+        atual->ant->prox = NULL;
+    }
+
+    // removendo no meio
+    else {
+        atual->ant->prox = atual->prox;
+        atual->prox->ant = atual->ant;
+    }
+
+    free(atual);
+    printf("Genero removido com sucesso!\n");
+    return 1;
+}
+
+//Funçoes do Igor
+
 Genero *iniciarGenero() {
     return NULL;
 }
