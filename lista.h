3b34774b54dcc4e2ea28b7b62eccdd39a626f320
laura_16 <lauraferreirasc.06@gmail.com>
2026-07-23 18:14:50 -0300

    Add artist and genre management functions

--- lista.h
+++ lista.h
@@ -23,4 +23,14 @@ int contarGeneros(Genero *g);
 void alterarGenero(Genero *g, char nomeAtual[], char novoNome[]);
 int removerGenero(Genero **g, char nome[]);
 
+//---------Laura--------
+typedef struct _artista Artista;
+typedef struct _genero Genero;
+Genero* buscar_genero(Genero *lista, const char *nome_genero);
+Genero* inserir_genero(Genero *lista_generos, const char *nome_genero);
+Artista* buscar_artista(Genero *lista_generos, const char *nome_artista);
+void inserir_artista(Genero *genero, const char *nome, const char *cidade, 
+                     const char *periodo, const char *obras, 
+                     const char *integrantes, const char *premiacoes);
+
 #endif
