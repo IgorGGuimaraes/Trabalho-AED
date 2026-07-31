16819881ce243b5cc295f8df5b580f29a7483b67
Igor Guimarães <152732516+IgorGGuimaraes@users.noreply.github.com>
2026-07-31 12:41:38 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -25,7 +25,7 @@ typedef struct Genero{
 Genero *iniciarGenero();
 void imprimirGeneros(Genero *g);
 int contarGeneros(Genero *g);
-void listarArtistas(Genero *g);
+void listarTodosArtistas(Genero *g);
 int contarArtistas(Genero *g);
 void listarArtistas(Genero *g);
 void filtrarArtistasPorCidade(Genero *g, char *cidade);
