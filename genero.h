5bad5a6945e2a9ba1d3f2249a2ba2fe71a95b0b3
rhuannh <rhuanhenrique809@gmail.com>
2026-08-05 13:18:25 -0300

    Update genero.h

--- genero.h
+++ genero.h
@@ -7,3 +7,12 @@ typedef struct Genero {
     Artista *artistas;
 } Genero;
 
+Genero *iniciarGenero();
+void carregarArquivo(Genero **lista);
+Genero *inserir_genero(Genero *lista_generos, const char *nome_genero);
+Genero *buscar_genero(Genero *lista, const char *nome_genero);
+void alterarGenero(Genero *g, char nomeAtual[], char novoNome[]);
+int removerGenero(Genero **g, char nome[]);
+void imprimirGeneros(Genero *g);
+int contarGeneros(Genero *g);
+
